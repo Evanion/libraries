e8324315d653fe12e73a0703ee62c8ac3ff74c2c
Mikael Pettersson <evanion@icloud.com>
2025-09-09 02:58:10 +0200

    feat: add docs site

---
+++ apps/docs/next.config.ts
@@ -0,0 +1,26 @@
+//@ts-check
+
+import { composePlugins, withNx } from '@nx/next';
+import nextra from 'nextra';
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  // Use this to set Nx-specific options
+  // See: https://nx.dev/recipes/next/next-config-setup
+  nx: {},
+};
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  withNextra,
+];
+
+export default composePlugins(...plugins)(nextConfig);
--- commitlint.config.js
+++ commitlint.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
